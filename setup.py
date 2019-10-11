dd5586b4206d8763f935af68293e39d823504e7e
torms3 <kisuklee@mit.edu>
2019-10-10 21:13:07 -0400

    modify origin url

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
-# from Cython.Build import cythonize
-# from distutils.sysconfig import get_python_inc
-# from setuptools import setup, Extension
 from setuptools import setup
 
 setup(
     name='dataprovider3',
     version='0.0.1',
     description='DataProvider3.',
-    url='https://github.com/torms3/DataProvider3',
+    url='https://github.com/seung-lab/DataProvider3',
     author='Kisuk Lee',
     author_email='kisuklee@mit.edu',
     license='MIT',
-    # requires=['cython'],
     packages=['dataprovider3',
               'dataprovider3.geometry',
               'dataprovider3.inference'],
